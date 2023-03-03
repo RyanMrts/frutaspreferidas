f06db9ac21b526cfda7342c0f846b8c2884894c7
miguel <miguelpositivo02@gmail.com>
2023-03-03 20:43:26 -0300

    Finalizar exercício |Frutaspreferidas2

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("MInhas Frutas Preferidas!");
+Console.WriteLine();
+Console.WriteLine("1-Melancia");
+Console.WriteLine("2-Ameixa");
+Console.WriteLine("3-Uva");
+Console.WriteLine("4-Carambola");
+Console.WriteLine("5-Maçã");
